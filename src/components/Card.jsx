a55dcad14874a527cc54c2f57a14940c488548c9
Kirill <dorc99@mail.ru>
2023-07-02 16:20:21 +0700

    добавил useMemo в card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo, useCallback } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 
 const Card = ({ propsCard, onCardClick, onCardLike, onCardDeleteOpenPopup }) => {
@@ -6,30 +6,33 @@ const Card = ({ propsCard, onCardClick, onCardLike, onCardDeleteOpenPopup }) => 
   const isOwn = propsCard.owner._id === currentUser._id;
   const isLiked = propsCard.likes.some((i) => i._id === currentUser._id);
 
-  function handleClick() {
+  const handleClick = () => {
     onCardClick(propsCard);
-  }
+  };
 
-  function handleLikeClick() {
+  const handleLikeClick = () => {
     onCardLike(propsCard);
-  }
+  };
 
-  function handleDeleteCard() {
-    onCardDeleteOpenPopup(propsCard)
-  }
+  const handleDeleteCard = () => {
+    onCardDeleteOpenPopup(propsCard);
+  };
 
-  return (
-    <article className="card">
-      <img src={propsCard.link} alt={propsCard.name} className="card__image" onClick={handleClick} />
-      <div className="card__wrap">
-        <h2 className="card__title">{propsCard.name}</h2>
-        <div className="card__like-container">
-          <button type="button" className={`card__like-image ${isLiked && "card__like-image_active"}`} onClick={handleLikeClick} />
-          <h2 className="card__like-current">{propsCard.likes.length}</h2>
+  const currentCard = useMemo(() => {
+    return (
+      <article className="card">
+        <img src={propsCard.link} alt={propsCard.name} className="card__image" onClick={handleClick} />
+        <div className="card__wrap">
+          <h2 className="card__title">{propsCard.name}</h2>
+          <div className="card__like-container">
+            <button type="button" className={`card__like-image ${isLiked && "card__like-image_active"}`} onClick={handleLikeClick} />
+            <h2 className="card__like-current">{propsCard.likes.length}</h2>
+          </div>
         </div>
-      </div>
-      {isOwn && <button type="button" className="card__trash-btn" onClick={handleDeleteCard} />}
-    </article>
-  );
+        {isOwn && <button type="button" className="card__trash-btn" onClick={handleDeleteCard} />}
+      </article>
+    );
+  }, [propsCard.likes.length, propsCard.name, propsCard.link, isLiked, isOwn]);
+  return currentCard;
 };
 export default Card;
